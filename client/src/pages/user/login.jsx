6c40fa17c9ce63764fe0a86f071aeb6854ef258a
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-06-10 10:34:28 +0800

    Login Form

--- client/src/pages/user/login.jsx
+++ client/src/pages/user/login.jsx
@@ -1,10 +1,57 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div>
-      <h2>Login</h2>
+    <>
+
+    <div className='flex items-center justify-center min-h-screen px-5'>
+
+      <div className='flex flex-col items-center justify-center gap-y-5 bg-white p-5 rounded-lg shadow-lg w-full max-w-md'>
+
+        <div className='text-center'>
+          <h2 className=' text-[26px] font-bold text-color'>HanaPark</h2>
+          <p className='text-color-2 text-sm'>Parking System</p>
+        </div>
+
+        <form className='flex flex-col gap-y-3  w-full'>
+
+          <input type="email"
+          required
+          name='email'
+          id='Email'
+          className='outline-0 border focus:border-color-3 p-2 rounded-md text-sm text-color-2'
+          placeholder='Email'/>  
+
+          <input type="password"
+          required
+          name='password'
+          id='Password'
+          className='outline-0 border focus:border-color-3 p-2 rounded-md text-sm text-color-2'
+          placeholder='Password' />
+
+        </form>
+
+        <div className='w-full'>
+
+          <button className='w-full bg-gradient-to-r from-blue-500 to-blue-900 text-white p-2 rounded-md hover:from-blue-600 hover:to-blue-800 transition duration-300 cursor-pointer text-sm'>
+            Sign In
+          </button>
+
+        </div>
+
+        <Link to='/reset-password' className='text-sm text-color'>
+          Forgot Password?
+        </Link>
+
+
+       
+      </div>
+
+      
+
     </div>
+    </>
   )
 }
 
